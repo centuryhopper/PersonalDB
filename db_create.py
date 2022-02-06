c80e9fa87c96cd18030839e897f94a799d413680
Leo Zhang <leotheasianlion@gmail.com>
2022-02-06 00:07:39 -0500

    Update db_create.py

--- db_create.py
+++ db_create.py
@@ -1,4 +1,9 @@
+import click
+from flask.cli import with_appcontext
+
 from app import db
 
-db.create_all()
-
+@click.command(name='create_tables')
+@with_appcontext
+def create_tables():
+    db.create_all()
