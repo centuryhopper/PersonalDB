6608421d537a1c244b7898cb9bc61789f9606c51
Leo Zhang <leotheasianlion@gmail.com>
2022-02-06 15:46:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+
 import os
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_restful import Api, Resource, abort, fields, marshal_with
@@ -9,12 +10,15 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL").replace("postgres://", "postgresql://", 1)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL").replace(
+    "postgres://", "postgresql://", 1
+)
 api = Api(app)
 
 db = SQLAlchemy(app)
 db.init_app(app)
 migrate = Migrate(app, db)
+
 
 class ContactModel(db.Model):
     _id = db.Column(db.Integer, primary_key=True)
@@ -25,7 +29,7 @@ class ContactModel(db.Model):
     relatedTopics = db.Column(db.String, nullable=False)
 
     def __repr__(self):
-        return f"Contact(_id = {_id}, firstName = {firstName}, lastName = {lastName}, email = {email}, phoneNumber = {phoneNumber}, relatedTopics = {relatedTopics})"
+        return f"Contact(_id = {self._id}, firstName = {self.firstName}, lastName = {self.lastName}, email = {self.email}, phoneNumber = {self.phoneNumber}, relatedTopics = {self.relatedTopics})"
 
 
 # how the video model should be serialized
@@ -65,56 +69,55 @@ class Contact(Resource):
             abort(404, message="Could not find contact with that id")
         return result
 
+    @marshal_with(resource_fields)
+    def put(self, contact_id):
+        args = put_args.parse_args()
+        result = ContactModel.query.filter_by(_id=contact_id).first()
+        if not result:
+            abort(404, message="Contact doesn't exist, cannot update")
+        if args['firstName']:
+            result.firstName = args['firstName']
+        if args['lastName']:
+            result.lastName = args['lastName']
+        if args['email']:
+            result.email = args['email']
+        if args['phoneNumber']:
+            result.phoneNumber = args['phoneNumber']
+        if args['relatedTopics']:
+            result.relatedTopics = args['relatedTopics']
+        db.session.commit()
+        return result
+
+    def delete(self, contact_id):
+        result = ContactModel.query.filter_by(_id=contact_id).first()
+        if not result:
+            abort(404, message="Contact doesn't exist, cannot delete")
+        db.session.delete(result)
+        db.session.commit()
+        return 'contact deleted', 204
 
 class ContactList(Resource):
     @marshal_with(resource_fields)
     def get(self):
         return ContactModel.query.all()
 
+@app.route("/api/v1/count")
+def count():
+    return str(len(ContactModel.query.all()))
 
-api.add_resource(Contact, "/contact/<int:contact_id>")
-api.add_resource(ContactList, "/contact/all")
-
-# def userExists(val:int):
-#     user_collection = mongo.db.Contacts
-#     query = user_collection.find_one({'_id' : val})
-#     return query is not None
+api.add_resource(Contact, "/api/v1/<int:contact_id>")
+api.add_resource(ContactList, "/api/v1/all")
 
 
-@app.route("/hello", methods=["GET", "POST", "PUT", "DELETE"])
+@app.route("/test", methods=["GET", "POST", "PUT", "DELETE"])
 def index():
-    return "<h1>Welcome to the main page!</h1>"
+    return "<h1>If you see this, then this api has been properly deployed</h1>"
 
-
-# @app.route('/getAll', methods=['GET'])
-# def getAll():
-#     res = [doc for doc in mongo.db.Contacts.find({})]
-#     # sort by _id in ascending order
-#     res.sort(key=lambda doc:doc['_id'])
-#     return json.dumps(res, sort_keys=True, indent=4, separators=(',', ': '))
 
 # @app.route('/countTotal', methods=['GET'])
 # def countTotal():
 #     return str(mongo.db.Contacts.count_documents({}))
 
-
-# @app.route('/put', methods=['PUT'])
-# def put():
-#     user_collection = mongo.db.Contacts
-#     args = put_args.parse_args()
-#     if not userExists(args['_id']):
-#         return '<h1>User with this id is not found!</h1>'
-#     if args['firstName']:
-#         user_collection.update_one({'_id' : args['_id']}, {'$set': {'firstName' : args['firstName']} })
-#     if args['lastName']:
-#         user_collection.update_one({'_id' : args['_id']}, {'$set': {'lastName' : args['lastName']} })
-#     if args['email']:
-#         user_collection.update_one({'_id' : args['_id']}, {'$set': {'email' : args['email']} })
-#     if args['phoneNumber']:
-#         user_collection.update_one({'_id' : args['_id']}, {'$set': {'phoneNumber' : args['phoneNumber']} })
-#     if args['relatedTopics']:
-#         user_collection.update_one({'_id' : args['_id']}, {'$set': {'relatedTopics' : args['relatedTopics']} })
-#     return '<h1>Updated a User!</h1>'
 
 # @app.route('/delete/id=<int:val>', methods=['DELETE'])
 # def delete(val):
@@ -129,5 +132,7 @@ def index():
 #     app.run(debug=False, host='0.0.0.0')
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
+
+
