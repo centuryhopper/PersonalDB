698e0a79d911caf3e76630b8f16bd0b71834ca89
Leo Zhang <leotheasianlion@gmail.com>
2022-02-05 22:37:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,10 +8,11 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
-app.config["DATABASE_URL"] = os.environ.get('POSTGRES_URL')
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("POSTGRES_URL")
 api = Api(app)
 
 db = SQLAlchemy(app)
+
 
 class ContactModel(db.Model):
     _id = db.Column(db.Integer, primary_key=True)
