ba61a3bf787d023f5584d9950a4e2dec7fc0fd8f
Leo Zhang <leotheasianlion@gmail.com>
2022-02-05 22:29:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///contacts.db"
+app.config["DATABASE_URL"] = os.environ.get('POSTGRES_URL')
 api = Api(app)
 
 db = SQLAlchemy(app)
